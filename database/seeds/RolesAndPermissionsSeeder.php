b4d8ba01362735659b66e967dbf405e5998e75e8
Imam Setiawan <s.imam29@gmail.com>
2022-05-31 17:30:12 +0700

    fix user role seeder

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -23,6 +23,42 @@ class RolesAndPermissionsSeeder extends Seeder
         DB::table('model_has_roles')->insert(
             [
                 [
+                    'member_id' => 1,
+                    'name' => 'Admin',
+                    'email' => 'admin@spesix-lms.com',
+                    'password' => '12345678'
+                ],
+                [
+                    'member_id' => 2,
+                    'name' => 'Supervisor',
+                    'email' => 'supervisor@spesix-lms.com',
+                    'password' => '12345678'
+                ],
+                [
+                    'member_id' => 3,
+                    'name' => 'Pengajar',
+                    'email' => 'teacher@spesix-lms.com',
+                    'password' => '12345678'
+                ],
+                [
+                    'grade_id' => 25,
+                    'member_id' => 4,
+                    'name' => 'Siswa',
+                    'email' => 'student@spesix-lms.com',
+                    'password' => '12345678'
+                ],
+                [
+                    'student_id' => 4,
+                    'name' => 'Wali Siswa',
+                    'email' => 'parent@spesix-lms.com',
+                    'password' => '12345678'
+                ]
+            ]
+        );
+
+        DB::table('model_has_roles')->insert(
+            [
+                [
                     'role_id' => '4',
                     'model_type' => 'App\User',
                     'model_id' => '1'
@@ -49,5 +85,6 @@ class RolesAndPermissionsSeeder extends Seeder
                 ]
             ]
         );
+
     }
 }
